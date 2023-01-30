f1543d698872975b144baf59e0df5322c7b83489
Sani Rogers <sanirogers138@gmail.com>
2023-01-29 19:19:30 -0500

    done react hooks jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I'm actually trying really hard to become a Fullstack Developer and get a bag.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
